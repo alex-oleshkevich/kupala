1bb9f3a96c15abf4cd633aaa7081a777cf06541d
alex.oleshkevich <alex.oleshkevich@gmail.com>
2021-05-10 19:30:04 +0300

    Add container class.

---
+++ kupala/container.py
@@ -0,0 +1,287 @@
+from __future__ import annotations
+
+import inspect
+import typing as t
+
+S = t.TypeVar("S", bound=type)
+N = t.Union[str, type]
+Factory = t.Callable[[t.Any], S]
+PostCreateHook = t.Callable[[S], None]
+
+
+class ContainerError(Exception):
+    """Base exception for all container errors."""
+
+
+class AliasExistsError(ContainerError):
+    """Raised when alias name is already taken."""
+
+
+class ServiceNotFound(ContainerError):
+    """Raised when container fails to retrieve service."""
+
+
+class ParameterUnsupported(ContainerError):
+    """Raised if function or constructor parameter is of positional type."""
+
+
+class NoTypeHintError(ContainerError):
+    """Raised if function or constructor parameter has no type hint."""
+
+
+class Binding:
+    def __init__(self, service: N, container: Container):
+        self._service = service
+        self._container = container
+
+    def alias(self, aliases: t.Union[N, list[N]]) -> Binding:
+        """Alias an existing services with an alternate name."""
+        self._container.alias(self._service, aliases)
+        return self
+
+    def tag(self, tags: t.Union[str, list[str]]) -> Binding:
+        """Assign tags to a service."""
+        self._container.tag(self._service, tags)
+        return self
+
+    def after_created(self, hook: PostCreateHook) -> Binding:
+        """Call a hook after the service has been created by factory."""
+        self._container.add_post_create_hook(self._service, hook)
+        return self
+
+
+class FactorySpec:
+    def __init__(self, factory: Factory, singleton: bool):
+        self.factory = factory
+        self.singleton = singleton
+
+
+class ByAlias:
+    def __init__(self, alias: str):
+        self.alias = alias
+
+
+def alias(alias: str) -> ByAlias:
+    """Inject a dependency using an alias."""
+    return ByAlias(alias)
+
+
+class ByTag:
+    def __init__(self, tag: str):
+        self.tag = tag
+
+
+def tag(tag: str) -> ByTag:
+    """Inject a list of dependencies for a specific tag."""
+    return ByTag(tag)
+
+
+class Container:
+    """A container for registered service instances and factories."""
+
+    def __init__(self) -> None:
+        self._instances: dict[N, t.Any] = {}
+        self._factories: dict[N, FactorySpec] = {}
+        self._aliases: dict[N, N] = {}
+        self._tags: dict[str, list[N]] = {}
+        self._post_create_hooks: dict[N, list[PostCreateHook]] = {}
+
+    @property
+    def aliases(self) -> dict[N, N]:
+        return self._aliases
+
+    @property
+    def tags(self) -> dict[str, list[N]]:
+        return self._tags
+
+    def get(self, name: N) -> S:
+        """Retrieve a service instance from the container."""
+        real_name = self.resolve(name)
+        if real_name not in self:
+            raise ServiceNotFound('Service "%s" is not registered.' % name)
+
+        if real_name in self._instances:
+            return self._instances[real_name]
+
+        factory = self._factories[real_name]
+        instance = self.invoke(factory.factory)
+        if factory.singleton:
+            self._instances[name] = instance
+        callbacks = self._post_create_hooks.get(name, [])
+        for callback in callbacks:
+            callback(instance)
+        return instance
+
+    def invoke(self, fn: t.Union[t.Callable, type], **args: t.Any) -> t.Any:
+        """Invoke a callable or class constructor injecting dependencies."""
+        signature = inspect.signature(fn)
+        if inspect.isclass(fn):
+            type_hints = t.get_type_hints(getattr(fn, "__init__"))
+        else:
+            type_hints = t.get_type_hints(fn)
+
+        injections = args
+        for p in signature.parameters.values():
+            if p.kind == inspect.Parameter.POSITIONAL_ONLY:
+                raise ParameterUnsupported(
+                    'Argument "%s" of "%s": '
+                    "positional only arguments not supported. " % (p.name, fn)
+                )
+
+            if p.name not in injections:
+                if p.name not in type_hints:
+                    raise NoTypeHintError(
+                        'Argument "%s" of "%s" has no type hint.' % (
+                            p.name, fn
+                        ))
+
+                dependency_type = type_hints[p.name]
+                resolved_dep: t.Any
+                if isinstance(dependency_type, ByAlias):
+                    resolved_dep = self.get(dependency_type.alias)
+                elif isinstance(dependency_type, ByTag):
+                    resolved_dep = self.get_by_tag(dependency_type.tag)
+                else:
+                    resolved_dep = self.get(dependency_type)
+                injections[p.name] = resolved_dep
+
+        return fn(**injections)
+
+    def bind(
+            self,
+            name: N,
+            instance: t.Any,
+            aliases: t.Union[N, list[N]] = None,
+            tags: t.Union[str, list[str]] = None,
+    ) -> Binding:
+        """Bind an instance to the container.
+        A cached instance always returned."""
+        self._instances[name] = instance
+        if aliases:
+            self.alias(name, aliases)
+        if tags:
+            self.tag(name, tags)
+        return Binding(name, self)
+
+    def factory(
+            self,
+            name: N,
+            factory: Factory,
+            singleton: bool = False,
+            aliases: t.Union[N, list[N]] = None,
+            tags: t.Union[str, list[str]] = None,
+    ) -> Binding:
+        """Bind a services factory.
+        The service factory is a callable that creates a service instance.
+        By default, it always creates a fresh instance unless `singleton`
+        is specified."""
+        self._factories[name] = FactorySpec(factory, singleton)
+        if aliases:
+            self.alias(name, aliases)
+        if tags:
+            self.tag(name, tags)
+        return Binding(name, self)
+
+    def singleton(
+            self,
+            name: t.Union[str, S],
+            factory: Factory,
+            aliases: t.Union[N, list[N]] = None,
+            tags: t.Union[str, list[str]] = None,
+    ) -> Binding:
+        """Bind a factory for a singleton service.
+        Each time you ask for a service, a cached instance will be returned."""
+        self.factory(name, factory, singleton=True, aliases=aliases, tags=tags)
+        return Binding(name, self)
+
+    def alias(self, service: N, aliases: t.Union[N, list[N]]) -> None:
+        """Alias an existing services with an alternate name."""
+        aliases = [aliases] if isinstance(aliases, (str, type)) else aliases
+        for alias_ in aliases:
+            if alias_ in self._aliases:
+                raise AliasExistsError(
+                    'Alias %s is taken by "%s" service.'
+                    % (
+                        alias_,
+                        self._aliases[alias_],
+                    )
+                )
+
+            real_name = self.resolve(service)
+            self._aliases[alias_] = real_name
+
+    def get_aliases(self, service: N) -> list[t.Union[str, type]]:
+        """Get all aliases assigned to the service."""
+        return [
+            alias for alias, real_name in self._aliases.items() if
+            real_name == service
+        ]
+
+    def tag(self, service: N, tags: t.Union[str, list[str]]) -> None:
+        """Assign tags to the service."""
+        tags = [tags] if isinstance(tags, str) else tags
+        for tag in tags:
+            self._tags.setdefault(tag, [])
+            self._tags[tag].append(service)
+
+    def get_by_tag(self, tag: str) -> list[t.Any]:
+        """Get services tagged with `tag`."""
+        return list(map(self.get, self._tags.get(tag, [])))
+
+    def get_tags(self, service: N) -> list[str]:
+        """Get all tags assigned to the service."""
+        return [tag for tag, services in self._tags.items() if
+                service in services]
+
+    def has(self, name: N) -> bool:
+        """Test if container contains service `name`."""
+        try:
+            real_name = self.resolve(name)
+            return any(
+                [
+                    real_name in self._instances,
+                    real_name in self._factories,
+                ]
+            )
+        except ServiceNotFound:
+            return False
+
+    def add_post_create_hook(self, name: N, hook: PostCreateHook) -> None:
+        """Add a callable to execute after successful service creation."""
+        self._post_create_hooks.setdefault(name, [])
+        self._post_create_hooks[name].append(hook)
+
+    def resolve(self, name: N) -> N:
+        """Resolve a service name or an alias into an actual service name."""
+        if name in self._aliases:
+            return self.resolve(self._aliases[name])
+
+        if not any([name in self._instances, name in self._factories]):
+            raise ServiceNotFound(
+                'Name "%s" not found in the container.' % name)
+        return name
+
+    def remove(self, name: N) -> None:
+        """Remove service from the container.
+        This will also remove all aliases and tags."""
+        if name in self._instances:
+            del self._instances[name]
+
+        if name in self._factories:
+            del self._factories
+
+        for tag in self.get_tags(name):
+            self._tags[tag].remove(name)
+            if len(self._tags[tag]) == 0:
+                del self._tags[tag]
+
+        for alias in self.get_aliases(name):
+            del self._aliases[alias]
+
+        if name in self._post_create_hooks:
+            del self._post_create_hooks[name]
+
+    __contains__ = has
+    __getitem__ = get
+    __setitem__ = bind
+    __delitem__ = remove
---
+++ tests/test_container.py
@@ -0,0 +1,235 @@
+from unittest import mock
+
+import pytest
+
+from kupala.container import alias
+from kupala.container import Container
+from kupala.container import NoTypeHintError
+from kupala.container import ParameterUnsupported
+from kupala.container import ServiceNotFound
+from kupala.container import tag
+
+
+class _Stub1:
+    ...
+
+
+@pytest.fixture
+def container():
+    return Container()
+
+
+def test_resolve(container):
+    container.bind("a", 1)
+    container.alias("a", "b")
+    container.alias("b", "c")
+
+    assert container.resolve("a") == "a"
+    assert container.resolve("b") == "a"
+    assert container.resolve("c") == "a"
+
+    with pytest.raises(ServiceNotFound):
+        assert container.resolve("unknown") == "unknown"
+
+
+def test_bind_has_get(container):
+    container.bind("a", 1)
+    assert "a" in container
+    assert container.get("a") == 1
+    assert container["a"] == 1
+
+    container["b"] = "b"
+    assert container["b"] == "b"
+    del container["b"]
+    assert "b" not in container
+
+
+def test_factory(container):
+    def factory() -> _Stub1:
+        return _Stub1()
+
+    container.factory(_Stub1, factory)
+    instance = container.get(_Stub1)
+    assert isinstance(instance, _Stub1)
+
+
+def test_factory_singleton(container):
+    def factory() -> _Stub1:
+        return _Stub1()
+
+    container.factory(_Stub1, factory, singleton=True)
+    instance = container.get(_Stub1)
+    assert container.get(_Stub1) == instance
+    assert container.get(_Stub1) == instance
+
+
+class _Stub2:
+    ...
+
+
+class _Stub3:
+    def __init__(self, stub: _Stub1, stub2: _Stub2):
+        self.stub = stub
+        self.stub2 = stub2
+
+
+def test_factory_injects(container):
+    def factory(stub: _Stub1, stub2: _Stub2) -> _Stub3:
+        return _Stub3(stub, stub2)
+
+    stub1 = _Stub1()
+    stub2 = _Stub2()
+    container.bind(_Stub1, stub1)
+    container.bind(_Stub2, stub2)
+    container.factory(_Stub3, factory)
+    instance = container.get(_Stub3)
+    assert isinstance(instance, _Stub3)
+    assert instance.stub == stub1
+    assert instance.stub2 == stub2
+
+
+def test_invoke(container):
+    container.bind(_Stub1, "stub1")
+    container.bind(_Stub2, "stub2")
+
+    def fn(s1: _Stub1, s2: _Stub2):
+        return [s1, s2]
+
+    assert container.invoke(fn) == ["stub1", "stub2"]
+
+
+def test_invoke_with_positional(container):
+    def fn(s1: _Stub1, /):
+        pass
+
+    with pytest.raises(ParameterUnsupported):
+        container.invoke(fn)
+
+
+def test_invoke_with_missing_type_hints(container):
+    def fn(s1):
+        pass
+
+    with pytest.raises(NoTypeHintError):
+        container.invoke(fn)
+
+
+def test_invoke_with_hints(container):
+    container.bind(_Stub1, "stub1")
+
+    def fn(s1: _Stub1, s2: _Stub2):
+        return [s1, s2]
+
+    assert container.invoke(fn, s2="stub2") == ["stub1", "stub2"]
+
+
+def test_invoke_creates_class_instance(container):
+    class _Dep:
+        def __init__(self, stub: _Stub1):
+            self.stub = stub
+
+    container.bind(_Stub1, "stub")
+    instance = container.invoke(_Dep)
+    assert instance.stub == "stub"
+
+
+def test_tag(container):
+    # service with one tag
+    container.bind("a", 1, tags="service.a")
+    assert container.get_by_tag("service.a") == [1]
+
+    # service with multiple tags
+    container.bind("b", 2, tags=["service.b", "service.b1"])
+    assert container.get_by_tag("service.b") == [2]
+    assert container.get_by_tag("service.b1") == [2]
+
+    # tag a service
+    container.tag("a", "service.c")
+    assert container.get_by_tag("service.c") == [1]
+
+    # tag a service with multiple tags
+    container.tag("a", ["service.d", "service.d1"])
+    assert container.get_by_tag("service.d") == [1]
+    assert container.get_by_tag("service.d1") == [1]
+
+    container.bind("d", 4, tags=["d1", "d2"])
+    assert container.get_tags("d") == ["d1", "d2"]
+
+
+def test_alias(container):
+    container.bind("a", 1, aliases="a1")
+    assert "a1" in container
+    assert container.get("a1") == 1
+
+    container.bind("b", 1, aliases=["b1", "b2"])
+    assert container.get("b1") == 1
+    assert container.get("b2") == 1
+
+    container.alias("a", "c")
+    assert container.get("c") == 1
+
+    container.bind("d", 4, aliases=["d1", "d2"])
+    container.alias("d2", "d3")
+    assert container.get_aliases("d") == ["d1", "d2", "d3"]
+
+
+def test_remove(container):
+    container.bind("a", 1, aliases=["a1", "a2"], tags=["ta1", "ta2"])
+    container.alias("a2", "b")
+
+    container.remove("a")
+    with pytest.raises(ServiceNotFound):
+        container.get("a1")
+    with pytest.raises(ServiceNotFound):
+        container.get("a2")
+
+    assert "a" not in container
+    assert "ta1" not in container.tags
+    assert "ta2" not in container.tags
+    assert "a1" not in container.aliases
+    assert "a2" not in container.aliases
+
+    # test __delitem__
+    container.bind("a", 1)
+    del container["a"]
+    assert "a" not in container
+
+
+def test_post_create_hooks(container):
+    def factory():
+        return 1
+
+    fn = mock.MagicMock()
+    fn2 = mock.MagicMock()
+    container.factory("f", factory).after_created(fn)
+    container.add_post_create_hook("f", fn2)
+    container.get("f")
+    fn.assert_called_once_with(1)
+    fn2.assert_called_once_with(1)
+
+
+def test_fluent_interface(container):
+    def hook():
+        ...
+
+    container.bind("a", 1).tag("t1").alias("a1").after_created(hook)
+    assert "t1" in container.tags
+    assert "a1" in container.aliases
+
+
+def test_injects_alias(container):
+    def fn(stub: alias("stub1")):
+        return stub
+
+    instance = _Stub1()
+    container.bind(_Stub1, instance, aliases="stub1")
+    assert container.invoke(fn) == instance
+
+
+def test_injects_by_tag(container):
+    def fn(stub: tag("stub1")):
+        return stub
+
+    instance = _Stub1()
+    container.bind(_Stub1, instance, tags="stub1")
+    assert container.invoke(fn) == [instance]
