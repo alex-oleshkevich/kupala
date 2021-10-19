38c78f2b2fb64205d2bd9bcfb44b9e1807e1058e
alex.oleshkevich <alex.oleshkevich@gmail.com>
2021-10-19 20:32:27 +0300

    Add flash messages.

---
+++ kupala/messages.py
@@ -0,0 +1,115 @@
+import abc
+import enum
+import typing as t
+from starlette.types import ASGIApp, Message, Receive, Scope, Send
+
+from kupala.requests import Request
+
+SCOPE_KEY = 'flash_messages'
+SESSION_KEY = 'flash_messages'
+
+
+class MessageCategory(enum.Enum):
+    DEBUG = 'debug'
+    INFO = 'info'
+    SUCCESS = 'success'
+    WARNING = 'warning'
+    ERROR = 'error'
+
+
+class FlashMessage(t.TypedDict):
+    category: str
+    message: str
+
+
+class FlashBag:
+    Category = MessageCategory
+
+    def __init__(self, messages: t.List[FlashMessage] = None):
+        self._messages = messages or []
+
+    def add(self, message: str, category: MessageCategory) -> None:
+        self._messages.append({'category': category.value, 'message': message})
+
+    def debug(self, message: str) -> None:
+        self.add(message, MessageCategory.DEBUG)
+
+    def info(self, message: str) -> None:
+        self.add(message, MessageCategory.INFO)
+
+    def success(self, message: str) -> None:
+        self.add(message, MessageCategory.SUCCESS)
+
+    def warning(self, message: str) -> None:
+        self.add(message, MessageCategory.WARNING)
+
+    def error(self, message: str) -> None:
+        self.add(message, MessageCategory.ERROR)
+
+    def all(self) -> t.List[FlashMessage]:
+        return self._messages
+
+    def stream(self) -> t.Generator[FlashMessage, None, None]:
+        while len(self._messages):
+            yield self._messages.pop(0)
+
+    def clear(self) -> None:
+        self._messages = []
+
+    def __len__(self) -> int:
+        return len(self._messages)
+
+
+class MessageStorage(abc.ABC):  # pragma: nocover
+    @abc.abstractmethod
+    def load(self, scope: Scope) -> FlashBag:
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def save(self, scope: Scope, bag: FlashBag) -> None:
+        raise NotImplementedError()
+
+
+class SessionStorage(MessageStorage):
+    def load(self, scope: Scope) -> FlashBag:
+        if 'session' not in scope:
+            raise KeyError('Sessions are disabled. Flash messages depend on SessionMiddleware.')
+
+        return FlashBag(scope['session'].get(SESSION_KEY, []))
+
+    def save(self, scope: Scope, bag: FlashBag) -> None:
+        scope['session'][SESSION_KEY] = bag.all()
+
+
+_storage_map: t.Dict[str, t.Type[MessageStorage]] = {
+    'session': SessionStorage,
+}
+
+
+class FlashMessagesMiddleware:
+    def __init__(self, app: ASGIApp, storage: t.Union[MessageStorage, t.Literal["session"]] = 'session') -> None:
+        self.app = app
+        if isinstance(storage, str):
+            storage_class = _storage_map[storage]
+            storage = storage_class()
+        self.storage = storage
+
+    async def __call__(self, scope: Scope, receive: Receive, send: Send) -> None:
+        if scope['type'] != 'http':  # pragma: nocover
+            await self.app(scope, receive, send)
+            return
+
+        bag = self.storage.load(scope)
+
+        async def send_wrapper(message: Message) -> None:
+            if message['type'] == 'http.response.start':
+                self.storage.save(scope, bag)
+            await send(message)
+
+        scope[SCOPE_KEY] = bag
+        await self.app(scope, receive, send_wrapper)
+
+
+def flash(request: Request) -> FlashBag:
+    assert SCOPE_KEY in request.scope, 'Flash messages require FlashMessagesMiddleware.'
+    return request.scope[SCOPE_KEY]
---
+++ tests/responses/test_messages.py
@@ -0,0 +1,85 @@
+import pytest
+import typing as t
+from starsessions import SessionMiddleware
+
+from kupala.application import Kupala
+from kupala.messages import FlashBag, FlashMessagesMiddleware, flash
+from kupala.requests import Request
+from kupala.responses import JSONResponse
+from kupala.testclient import TestClient
+
+
+@pytest.mark.parametrize('storage', ['session'])
+def test_flash_messages(storage: t.Literal['session']) -> None:
+    def set_view(request: Request) -> JSONResponse:
+        flash(request).success('This is a message.')
+        return JSONResponse({})
+
+    def get_view(request: Request) -> JSONResponse:
+        bag = flash(request)
+        return JSONResponse({'messages': list(bag.stream())})
+
+    app = Kupala()
+    app.routes.post('/set', set_view)
+    app.routes.get('/get', get_view)
+
+    client = TestClient(
+        SessionMiddleware(
+            FlashMessagesMiddleware(app, storage=storage),
+            secret_key='key!',
+            autoload=True,
+        )
+    )
+    client.post('/set')
+
+    response = client.get('/get')
+    assert response.json()['messages'] == [{'category': 'success', 'message': 'This is a message.'}]
+
+    # must be empty after reading messages
+    response = client.get('/get')
+    assert response.json()['messages'] == []
+
+
+def test_flash_messages_session_storages_requires_session() -> None:
+    app = Kupala()
+
+    with pytest.raises(KeyError) as ex:
+        client = TestClient(FlashMessagesMiddleware(app, storage='session'))
+        client.get('/')
+    assert ex.value.args[0] == 'Sessions are disabled. Flash messages depend on SessionMiddleware.'
+
+
+def test_flash_messages_requires_flashmessages_middleware() -> None:
+    request = Request(
+        {
+            'type': 'http',
+        }
+    )
+    with pytest.raises(AssertionError) as ex:
+        flash(request)
+    assert str(ex.value) == 'Flash messages require FlashMessagesMiddleware.'
+
+
+def test_flash_bag() -> None:
+    bag = FlashBag()
+    bag.add('success', FlashBag.Category.SUCCESS)
+    bag.success('success')
+    bag.error('error')
+    bag.warning('warning')
+    bag.info('info')
+    bag.debug('debug')
+    assert len(bag) == 6
+
+    bag.clear()
+    assert len(bag) == 0
+
+
+def test_flash_bag_stream() -> None:
+    bag = FlashBag()
+    bag.success('one')
+    bag.success('two')
+
+    gen = bag.stream()
+    assert next(gen)['message'] == 'one'
+    assert next(gen)['message'] == 'two'
+    assert len(bag) == 0
