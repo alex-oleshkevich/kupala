364b41e23eb238bbd69e6b3593557b54f58cc18c
alex.oleshkevich <alex.oleshkevich@gmail.com>
2021-11-07 02:52:16 +0300

    Add form errors and old input.

--- kupala/requests.py
+++ kupala/requests.py
@@ -4,12 +4,51 @@ import re
 import typing as t
 from imia import UserToken
 from starlette import requests
+from starlette.datastructures import UploadFile
 from starlette.requests import empty_receive, empty_send
 from starlette.types import Receive, Scope, Send
 from starsessions import Session
 
 if t.TYPE_CHECKING:
     from .application import Kupala
+
+
+class OldFormInput(t.Mapping):
+    def __init__(self, data: dict) -> None:
+        self._data = data
+
+    def get(self, key: str, default: t.Any = None) -> t.Any:
+        return self._data.get(key, default)
+
+    def __getitem__(self, item: str) -> t.Any:
+        return self._data[item]
+
+    def __len__(self) -> int:
+        return len(self._data)
+
+    def __iter__(self) -> t.Iterator[t.Tuple[str, t.Any]]:
+        return iter(self._data)
+
+    def __contains__(self, item: object) -> bool:
+        return item in self._data
+
+    def to_json(self) -> dict:
+        return self._data
+
+
+class FormErrors:
+    def __init__(self, errors: t.Mapping, error_message: str) -> None:
+        self._errors = errors
+        self._error_message = error_message
+
+    def to_json(self) -> dict:
+        return {
+            'message': self._error_message,
+            'field_errors': self._errors,
+        }
+
+    def __bool__(self) -> bool:
+        return bool(self._errors or self._error_message)
 
 
 class Request(requests.Request):
@@ -65,6 +104,30 @@ class Request(requests.Request):
     def session(self) -> Session:
         assert "session" in self.scope, "SessionMiddleware must be installed to access request.session"
         return self.scope['session']
+
+    @property
+    def old_input(self) -> OldFormInput:
+        """Get previous form input. This value does not include uploaded files."""
+        return OldFormInput(self.session.pop('_form_old_input', {}))
+
+    async def remember_form_data(self) -> None:
+        """Flush current form data into session so it can be used on the next page to render form errors."""
+        data = await self.form()
+        self.session['_form_old_input'] = {k: v for k, v in data.items() if not isinstance(v, UploadFile)}
+
+    @property
+    def form_errors(self) -> FormErrors:
+        """Get form errors generated on the previous page."""
+        return FormErrors(
+            errors=self.session.pop('_form_field_errors'),
+            error_message=self.session.pop('_form_error'),
+        )
+
+    def set_form_errors(self, error_message: str = '', field_errors: dict = None) -> None:
+        """Flush form error message and form field errors into session.
+        This data can be later retrieved via `request.form_errors` attribute."""
+        self.session['_form_error'] = error_message
+        self.session['_form_field_errors'] = field_errors or {}
 
     def url_matches(self, *patterns: t.Union[str, t.Pattern]) -> bool:
         for pattern in patterns:
---
+++ tests/test_old_input.py
@@ -0,0 +1,84 @@
+from starsessions import SessionMiddleware
+
+from kupala.application import Kupala
+from kupala.requests import Request
+from kupala.responses import JSONResponse, RedirectResponse
+from kupala.routing import Route
+from kupala.testclient import TestClient
+
+
+async def set_view(request: Request) -> RedirectResponse:
+    await request.remember_form_data()
+    return RedirectResponse('/get')
+
+
+def get_view(request: Request) -> JSONResponse:
+    return JSONResponse(request.old_input)
+
+
+def test_old_input() -> None:
+    app = Kupala(routes=[Route('/', set_view, methods=['POST']), Route('/get', get_view)])
+    app.middleware.use(SessionMiddleware, secret_key='key', autoload=True)
+    client = TestClient(app)
+
+    with open('/tmp/test1.txt', 'wb') as f:
+        f.write(b'content')
+
+    with open('/tmp/test1.txt', 'rb') as f:
+        response = client.post(
+            '/',
+            allow_redirects=True,
+            data=[('first_name', 'root'), ('last_name', 'user')],
+            files=[{'avatar', f}],
+        )
+        assert response.status_code == 200
+        assert response.json() == {
+            'first_name': 'root',
+            'last_name': 'user',
+        }
+
+    # when accessing page for the second time, the session data has to be absent
+    response = client.get('/get')
+    assert response.json() == {}
+
+
+async def set_form_errors_view(request: Request) -> RedirectResponse:
+    request.set_form_errors(
+        'Form is invalid.',
+        {
+            'first_name': ['This field is required.'],
+            'last_name': ['This field is required.'],
+        },
+    )
+    return RedirectResponse('/get')
+
+
+def get_form_errors_view(request: Request) -> JSONResponse:
+    return JSONResponse(request.form_errors)
+
+
+def test_form_errors() -> None:
+    app = Kupala(routes=[Route('/', set_form_errors_view, methods=['POST']), Route('/get', get_form_errors_view)])
+    app.middleware.use(SessionMiddleware, secret_key='key', autoload=True)
+    client = TestClient(app)
+
+    response = client.post(
+        '/',
+        allow_redirects=True,
+        data={
+            'first_name': 'root',
+            'last_name': 'user',
+        },
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        'message': 'Form is invalid.',
+        'field_errors': {
+            'first_name': ['This field is required.'],
+            'last_name': ['This field is required.'],
+        },
+    }
+
+    # when accessing page for the second time, the session data has to be absent
+    response = client.get('/get')
+    assert response.json() == {'message': None, 'field_errors': None}
