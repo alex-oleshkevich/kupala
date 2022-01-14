d581396d3fa0cbcce527ccf9d3993a73f7339101
alex.oleshkevich <alex.oleshkevich@gmail.com>
2022-01-14 17:19:38 +0300

    Add get_request_injection_factory and get_app_injection_factory utilities.

--- kupala/di.py
+++ kupala/di.py
@@ -45,7 +45,7 @@ class Injector:
         It will look up `from_app` class method on `klass` and call it if found.
         If `from_app` is not implemented then it will look up preferences,
         and it will raise `InjectionError` if nothing helped."""
-        if callback := getattr(klass, 'from_app', getattr(klass, _FACTORY_ATTR, None)):
+        if callback := get_app_injection_factory(klass):
             return callback(self.app)
 
         if klass in self.preferences:
@@ -54,10 +54,6 @@ class Injector:
             return self.preferences[klass]
 
         raise InjectionError(f'Factory for type "{klass.__name__}" cannot be found in DI configuration.')
-
-
-def get_request_injectable_key() -> str:
-    return _REQUEST_FACTORY_ATTR
 
 
 def injectable(factory: typing.Callable[[Kupala], _T]) -> typing.Callable[[typing.Type[_T]], typing.Type[_T]]:
@@ -84,3 +80,13 @@ def to_request_injectable(klass: typing.Type[_T], factory: typing.Callable[[Requ
 def to_app_injectable(klass: typing.Type[_T], factory: typing.Callable[[Kupala], _T]) -> None:
     """Convert regular class into app injectable."""
     injectable(factory=factory)(klass)
+
+
+def get_request_injection_factory(klass: typing.Type) -> typing.Callable[[typing.Any], typing.Any] | None:
+    """Retrieve factory callable from `klass`. This factory can create new instances from request."""
+    return getattr(klass, 'from_request', getattr(klass, _REQUEST_FACTORY_ATTR, None))
+
+
+def get_app_injection_factory(klass: typing.Type) -> typing.Callable[[typing.Any], typing.Any] | None:
+    """Retrieve factory callable from `klass`. This factory can create new instances from app instance."""
+    return getattr(klass, 'from_app', getattr(klass, _FACTORY_ATTR, None))
