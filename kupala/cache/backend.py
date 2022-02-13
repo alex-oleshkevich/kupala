51dd304830eaaf6601e158e02b1e8ad1fc847976
alex.oleshkevich <alex.oleshkevich@gmail.com>
2022-02-14 01:29:07 +0300

    Add ABC for cache backend.

--- kupala/cache/backend.py
+++ kupala/cache/backend.py
@@ -1,33 +1,44 @@
+import abc
 import typing
 
 
-class CacheBackend:
+class CacheBackend(abc.ABC):
+    @abc.abstractmethod
     async def get(self, key: str) -> typing.Any:
         ...
 
+    @abc.abstractmethod
     async def get_many(self, keys: typing.Iterable[str]) -> dict[str, typing.Any]:
         ...
 
+    @abc.abstractmethod
     async def set(self, key: str, ttl: int) -> None:
         ...
 
+    @abc.abstractmethod
     async def set_many(self, value: dict[str, typing.Any], ttl: int) -> None:
         ...
 
+    @abc.abstractmethod
     async def delete(self, key: str) -> None:
         ...
 
+    @abc.abstractmethod
     async def delete_many(self, keys: typing.Iterable[str]) -> None:
         ...
 
+    @abc.abstractmethod
     async def clear(self) -> None:
         ...
 
+    @abc.abstractmethod
     async def increment(self, key: str, step: int) -> None:
         ...
 
+    @abc.abstractmethod
     async def decrement(self, key: str, step: int) -> None:
         ...
 
+    @abc.abstractmethod
     async def touch(self, key: str, delta: int) -> None:
         ...
--- kupala/cache/memory.py
+++ kupala/cache/memory.py
@@ -1,5 +1,35 @@
+import typing
+
 from kupala.cache.backend import CacheBackend
 
 
 class InMemoryCache(CacheBackend):
-    ...
+    async def get(self, key: str) -> typing.Any:
+        pass
+
+    async def get_many(self, keys: typing.Iterable[str]) -> dict[str, typing.Any]:
+        pass
+
+    async def set(self, key: str, ttl: int) -> None:
+        pass
+
+    async def set_many(self, value: dict[str, typing.Any], ttl: int) -> None:
+        pass
+
+    async def delete(self, key: str) -> None:
+        pass
+
+    async def delete_many(self, keys: typing.Iterable[str]) -> None:
+        pass
+
+    async def clear(self) -> None:
+        pass
+
+    async def increment(self, key: str, step: int) -> None:
+        pass
+
+    async def decrement(self, key: str, step: int) -> None:
+        pass
+
+    async def touch(self, key: str, delta: int) -> None:
+        pass
