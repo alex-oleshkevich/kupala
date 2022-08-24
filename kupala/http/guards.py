87bcaf538ae3e69f7e9846fcf5f6419918f4ad01
alex.oleshkevich <alex.oleshkevich@gmail.com>
2022-08-24 16:02:47 +0300

    fix imports

--- kupala/http/guards.py
+++ kupala/http/guards.py
@@ -3,8 +3,8 @@ from __future__ import annotations
 import inspect
 import typing
 
-from kupala.http import Response
 from kupala.http.requests import Request
+from kupala.http.responses import Response
 
 
 class Guard(typing.Protocol):  # pragma: no cover
