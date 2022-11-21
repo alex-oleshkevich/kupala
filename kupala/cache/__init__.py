58ab1cd6b263bee9217d3b2b21da542bbb06292d
alex.oleshkevich <alex.oleshkevich@gmail.com>
2022-11-21 11:39:04 +0300

    remove cache framework

--- kupala/cache/__init__.py
+++
@@ -1,5 +0,0 @@
-from kupala.cache.backends import CacheBackend, DummyCache, FileCache, InMemoryCache
-
-from .cache import Cache
-
-__all__ = ["Cache", "CacheBackend", "InMemoryCache", "DummyCache", "FileCache"]
--- kupala/cache/backends/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base import CacheBackend
-from .dummy import DummyCache
-from .file import FileCache
-from .memory import InMemoryCache
-
-__all__ = ["CacheBackend", "FileCache", "DummyCache", "InMemoryCache"]
