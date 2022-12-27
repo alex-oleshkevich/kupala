6b5f7e5d17321ca819fe84e4faf1cefe517bfb8a
alex.oleshkevich <alex.oleshkevich@gmail.com>
2022-12-27 12:41:11 +0300

    add storages helper

---
+++ tests/test_storages.py
@@ -0,0 +1,22 @@
+from starlette.datastructures import UploadFile
+
+from kupala.storages import generate_file_name
+
+
+def test_generate_file_name() -> None:
+    upload = UploadFile("sample.txt")
+    assert generate_file_name(upload, "/uploads/{name}.{ext}") == "/uploads/sample.txt"
+
+
+def test_generate_file_name_with_custom_tokens() -> None:
+    upload = UploadFile("sample.txt")
+    assert generate_file_name(upload, "/uploads/{pk}/{name}.{ext}", extra_tokens={"pk": 1}) == "/uploads/1/sample.txt"
+
+
+def test_generate_file_name_with_callable_destination() -> None:
+    upload = UploadFile("sample.txt")
+
+    def destination(file: UploadFile) -> str:
+        return "/uploads/{name}.{ext}"
+
+    assert generate_file_name(upload, destination) == "/uploads/sample.txt"
