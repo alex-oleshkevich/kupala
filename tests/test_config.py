1a1fb434e1dd2d4967faa82d8a2d22515c53bbc4
alex.oleshkevich <alex.oleshkevich@gmail.com>
2022-12-29 20:12:59 +0300

    add secrets

---
+++ tests/test_config.py
@@ -0,0 +1,18 @@
+import pathlib
+import pytest
+
+from kupala.config import Secrets
+
+
+def test_secrets(tmp_path: pathlib.Path) -> None:
+    secret_file = tmp_path / "value.secret"
+    secret_file.write_text("1")
+
+    secrets = Secrets(tmp_path)
+
+    assert secrets.get("value.secret") == "1"
+    assert secrets.get("missing.secret", "default") == "default"
+    assert secrets.get("value.secret", cast=int) == 1
+
+    with pytest.raises(FileNotFoundError, match="Secret file missing"):
+        secrets.get("missing.secret")
