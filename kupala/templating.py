fc08f98c0203cb1a9e6e8f9f8a4210463b663a3d
alex.oleshkevich <alex.oleshkevich@gmail.com>
2023-01-03 13:47:45 +0300

    improve templating

--- kupala/templating.py
+++ kupala/templating.py
@@ -1,15 +1,17 @@
 from __future__ import annotations
 
 import functools
+import glob
 import jinja2
+import os
 import time
 import typing
 from jinja2.runtime import Macro
 from starlette import responses, templating
+from starlette.applications import Starlette
 from starlette.background import BackgroundTask
 from starlette.datastructures import URL
 from starlette.requests import Request
-from starlette.responses import Response
 
 _boot_time = time.time()
 
@@ -17,51 +19,6 @@ _boot_time = time.time()
 class ContextProcessor(typing.Protocol):  # pragma: nocover
     def __call__(self, request: Request) -> typing.Mapping:
         ...
-
-
-class _Registry:
-    def __init__(self) -> None:
-        self.items: dict[str, typing.Callable] = {}
-
-    def update(self, filters: dict[str, typing.Any]) -> None:
-        self.items.update(filters)
-
-    def add(self, name: str, callback: typing.Callable) -> None:
-        self.items[name] = callback
-
-    def register(self, name: str) -> typing.Callable:
-        def inner_decorator(fn: typing.Callable) -> typing.Callable:
-            self.add(name, fn)
-            return fn
-
-        return inner_decorator
-
-    def keys(self) -> typing.Iterable[str]:
-        return self.items.keys()
-
-    def __getitem__(self, item: str) -> typing.Any:
-        return self.items[item]
-
-    def __iter__(self) -> typing.Iterator[str]:
-        return iter(self.items)
-
-    def __len__(self) -> int:
-        return len(self.items)
-
-
-class Library:
-    def __init__(self) -> None:
-        self.filters = _Registry()
-        self.tests = _Registry()
-
-
-class DynamicChoiceLoader(jinja2.ChoiceLoader):
-    loaders: list
-
-    def add_loader(self, loader: jinja2.BaseLoader) -> None:
-        # don't touch the first loader, this is usually project's template directory
-        # also, don't append it because the last loader should be one that loads templates from the framework
-        self.loaders.insert(1, loader)
 
 
 def media_url(request: Request, path: str, path_name: str = "media") -> str:
@@ -89,15 +46,18 @@ def abs_url_for(request: Request, path_name: str, **path_params: typing.Any) -> 
 def url_matches(
     request: Request, path_name: str, path_params: dict[str, str | int] | None = None, exact: bool = False
 ) -> bool:
-    target_url = request.app.router.url_path_for(path_name, **(path_params or {}))
+    target_url = str(request.app.router.url_path_for(path_name, **(path_params or {})))
     if exact:
         return target_url == request.url.path
-    return target_url in request.url.path
+    return request.url.path.startswith(target_url)
 
 
-def framework_processors(request: Request) -> dict[str, typing.Any]:
+def app_processor(request: Request) -> dict[str, typing.Any]:
+    return {"app": request.app}
+
+
+def url_processors(request: Request) -> dict[str, typing.Any]:
     return {
-        "app": request.app,
         "url": functools.partial(url_for, request),
         "abs_url": functools.partial(abs_url_for, request),
         "static_url": functools.partial(static_url, request),
@@ -106,12 +66,73 @@ def framework_processors(request: Request) -> dict[str, typing.Any]:
     }
 
 
-class Jinja2Templates:
-    def __init__(self, jinja_env: jinja2.Environment, context_processors: list[ContextProcessor] | None = None) -> None:
-        self.jinja_env = jinja_env
-        self.context_processors = context_processors or []
-        self._templates = templating.Jinja2Templates(directory="templates")
-        self._templates.env = jinja_env
+class Jinja2Templates(templating.Jinja2Templates):
+    """Integrate Jinja templates."""
+
+    env: jinja2.Environment
+
+    def __init__(
+        self,
+        template_dir: str | os.PathLike | list[str | os.PathLike] = "templates",
+        packages: list[str] | None = None,
+        env: jinja2.Environment | None = None,
+        loader: jinja2.BaseLoader | None = None,
+        filters: dict[str, typing.Callable] | None = None,
+        tests: dict[str, typing.Callable] | None = None,
+        globals: dict[str, typing.Any] | None = None,
+        context_processors: list[ContextProcessor] | None = None,
+        plugins: list[typing.Callable[[jinja2.Environment], None]] | None = None,
+        **jinja_env_options: typing.Any,
+    ) -> None:
+        self._context_processors = context_processors or []
+        packages = packages or []
+        packages.extend(["starlette_flash"])
+
+        template_directories: list[str] = []
+        for template_dir in [template_dir] if isinstance(template_dir, (str, os.PathLike)) else template_dir:
+            directory = glob.glob(str(template_dir))
+            template_directories.extend(directory)
+
+        loader = loader or jinja2.ChoiceLoader(
+            [
+                jinja2.FileSystemLoader(template_directories),
+                *[jinja2.PackageLoader(package_dir) for package_dir in packages],
+            ]
+        )
+
+        super().__init__("templates", loader=loader, **jinja_env_options)
+        self.env = env or self.env
+        self.env.globals.update(globals or {})
+        self.env.filters.update(filters or {})
+        self.env.tests.update(tests or {})
+
+        for plugin in plugins or []:
+            plugin(self.env)
+
+    def macro(self, template_name: str, macro_name: str) -> Macro:
+        """Return a macros instance from a template."""
+        return getattr(self.get_template(template_name).module, macro_name)
+
+    def render_to_string(self, template_name: str, context: dict[str, typing.Any] | None = None) -> str:
+        """Render template to string."""
+        template = self.get_template(template_name)
+        return template.render(context or {})
+
+    def render_block_to_string(
+        self, template_name: str, block_name: str, context: dict[str, typing.Any] | None = None
+    ) -> str:
+        """Render template block to string."""
+        template = self.get_template(template_name)
+        block = template.blocks[block_name]
+        block_context = template.new_context(context)
+        return "".join(block(block_context))
+
+    def render_macro_to_string(
+        self, template_name: str, macro_name: str, macro_kwargs: dict[str, typing.Any] | None = None
+    ) -> str:
+        """Render template macro to string."""
+        macro = self.macro(template_name, macro_name)
+        return macro(**(macro_kwargs or {}))
 
     def TemplateResponse(
         self,
@@ -122,16 +143,15 @@ class Jinja2Templates:
         status_code: int = 200,
         headers: dict[str, typing.Any] | None = None,
         content_type: str = "text/html",
-        context_processors: list[ContextProcessor] | None = None,
         background: BackgroundTask | None = None,
     ) -> responses.Response:
+        """Render template to HTTP response."""
         context = context or {}
         context["request"] = request
-        context_processors = context_processors or []
-        for processor in [*self.context_processors, *context_processors]:
+        for processor in self._context_processors:
             context.update(processor(request))
 
-        return self._templates.TemplateResponse(
+        return super().TemplateResponse(
             name=template_name,
             context=context,
             status_code=status_code,
@@ -140,42 +160,34 @@ class Jinja2Templates:
             background=background,
         )
 
-    def TemplateMacroResponse(
-        self,
-        template_name: str,
-        macro_name: str,
-        macro_kwargs: dict[str, typing.Any] | None = None,
-        *,
-        status_code: int = 200,
-        headers: dict[str, typing.Any] | None = None,
-        content_type: str = "text/html",
-        background: BackgroundTask | None = None,
-    ) -> Response:
-        content = self.render_macro_to_string(template_name, macro_name, macro_kwargs)
-        return Response(
-            content=content, status_code=status_code, headers=headers, media_type=content_type, background=background
-        )
+    def context_processor(self, fn: ContextProcessor) -> ContextProcessor:
+        """Add a context processor."""
+        self._context_processors.append(fn)
+        return fn
 
-    def macro(self, template_name: str, macro_name: str) -> Macro:
-        return getattr(self.get_template(template_name).module, macro_name)
+    def filter(self, name: str = "") -> typing.Callable:
+        """Add a new filter to jinja2 environment."""
 
-    def get_template(self, template_name: str) -> jinja2.Template:
-        return self._templates.get_template(template_name)
+        def decorator(fn: typing.Callable) -> typing.Callable:
+            nonlocal name
+            name = name or fn.__name__
+            self.env.filters[name] = fn
+            return fn
 
-    def render_to_string(self, template_name: str, context: dict[str, typing.Any] | None = None) -> str:
-        template = self.get_template(template_name)
-        return template.render(context or {})
+        return decorator
 
-    def render_block_to_string(
-        self, template_name: str, block_name: str, context: dict[str, typing.Any] | None = None
-    ) -> str:
-        template = self.get_template(template_name)
-        block = template.blocks[block_name]
-        block_context = template.new_context(context)
-        return "".join(block(block_context))
+    def test(self, name: str = "") -> typing.Callable:
+        """Add a new test to jinja2 environment."""
 
-    def render_macro_to_string(
-        self, template_name: str, macro_name: str, macro_kwargs: dict[str, typing.Any] | None = None
-    ) -> str:
-        macro = self.macro(template_name, macro_name)
-        return macro(**(macro_kwargs or {}))
+        def decorator(fn: typing.Callable) -> typing.Callable:
+            nonlocal name
+            name = name or fn.__name__
+            self.env.tests[name] = fn
+            return fn
+
+        return decorator
+
+    def setup(self, app: Starlette) -> None:
+        """Integrate templates and Starlette app."""
+        app.state.templates = self
+        app.state.jinja_env = self.env
--- tests/test_templating.py
+++ tests/test_templating.py
@@ -1,16 +1,24 @@
 import jinja2
+import pathlib
 import pytest
 import typing
 from pathlib import Path
+from starlette.applications import Starlette
 from starlette.datastructures import MutableHeaders
+from starlette.routing import Mount
+from starlette.types import Receive, Scope, Send
 
 from kupala.requests import Request
-from kupala.templating import Jinja2Templates, Library
+from kupala.templating import Jinja2Templates, abs_url_for, media_url, static_url, url_for, url_matches
 
 template = """SIMPLE TEXT
 FROM CONTEXT {{ context_variable }}
 {% block main %}MAIN BLOCK CONTENT {{ block_var }}{% endblock %}
 {% macro feature(name) %}MACRO {{ name }}{% endmacro %}"""
+
+
+async def simple_asgi_app(scope: Scope, receive: Receive, send: Send) -> None:
+    pass
 
 
 @pytest.fixture
@@ -25,30 +33,9 @@ def jinja_env(templates: Path) -> jinja2.Environment:
     return jinja2.Environment(loader=jinja2.FileSystemLoader(templates))
 
 
-@pytest.fixture
-def library() -> Library:
-    return Library()
-
-
-def test_library_registry(library: Library) -> None:
-    @library.filters.register("simple")
-    def simple_filter(value: typing.Any) -> typing.Any:
-        return value
-
-    assert "simple" in library.filters.items
-
-
-def test_library_registry_update(library: Library) -> None:
-    def simple_filter(value: typing.Any) -> typing.Any:
-        return value
-
-    library.filters.update({"simple": simple_filter})
-    assert "simple" in library.filters.items
-
-
 def test_render_to_response(jinja_env: jinja2.Environment) -> None:
     request = Request(scope={"type": "http"})
-    templates = Jinja2Templates(jinja_env)
+    templates = Jinja2Templates(env=jinja_env)
     response = templates.TemplateResponse(
         request,
         "index.html",
@@ -66,42 +53,239 @@ def test_render_to_response(jinja_env: jinja2.Environment) -> None:
     )
 
 
-def test_render_to_response_with_context_processors(jinja_env: jinja2.Environment) -> None:
+def test_context_processors(jinja_env: jinja2.Environment) -> None:
     def context_processor(request: Request) -> dict[str, typing.Any]:
         return {"context_variable": "from_context"}
 
     request = Request(scope={"type": "http"})
-    templates = Jinja2Templates(jinja_env, context_processors=[context_processor])
+    templates = Jinja2Templates(env=jinja_env, context_processors=[context_processor])
     response = templates.TemplateResponse(request, "index.html")
     assert response.body == b"SIMPLE TEXT\nFROM CONTEXT from_context\nMAIN BLOCK CONTENT \n"
 
 
-def test_render_to_response_with_inline_context_processors(jinja_env: jinja2.Environment) -> None:
-    def context_processor(request: Request) -> dict[str, typing.Any]:
-        return {"context_variable": "from_context"}
-
-    request = Request(scope={"type": "http"})
-    templates = Jinja2Templates(jinja_env)
-    response = templates.TemplateResponse(request, "index.html", context_processors=[context_processor])
-    assert response.body == b"SIMPLE TEXT\nFROM CONTEXT from_context\nMAIN BLOCK CONTENT \n"
-
-
-def test_render_macro_to_response(jinja_env: jinja2.Environment) -> None:
-    templates = Jinja2Templates(jinja_env)
-    response = templates.TemplateMacroResponse("index.html", "feature", macro_kwargs={"name": "value"})
-    assert response.body == b"MACRO value"
-
-
 def test_render_to_string(jinja_env: jinja2.Environment) -> None:
-    templates = Jinja2Templates(jinja_env)
+    templates = Jinja2Templates(env=jinja_env)
     assert templates.render_to_string("index.html") == "SIMPLE TEXT\nFROM CONTEXT \nMAIN BLOCK CONTENT \n"
 
 
 def test_render_block_to_string(jinja_env: jinja2.Environment) -> None:
-    templates = Jinja2Templates(jinja_env)
+    templates = Jinja2Templates(env=jinja_env)
     assert templates.render_block_to_string("index.html", "main") == "MAIN BLOCK CONTENT "
 
 
 def test_render_macro_to_string(jinja_env: jinja2.Environment) -> None:
-    templates = Jinja2Templates(jinja_env)
+    templates = Jinja2Templates(env=jinja_env)
     assert templates.render_macro_to_string("index.html", "feature", {"name": "value"}) == "MACRO value"
+
+
+def test_accepts_single_template_dir(jinja_env: jinja2.Environment, tmp_path: pathlib.Path) -> None:
+    (tmp_path / "index.html").write_text("hello")
+    templates = Jinja2Templates(template_dir=tmp_path)
+    assert templates.render_to_string("index.html") == "hello"
+
+    templates = Jinja2Templates(template_dir=str(tmp_path))
+    assert templates.render_to_string("index.html") == "hello"
+
+
+def test_accepts_single_template_dir_as_list(jinja_env: jinja2.Environment, tmp_path: pathlib.Path) -> None:
+    (tmp_path / "index.html").write_text("hello")
+    templates = Jinja2Templates(template_dir=[tmp_path])
+    assert templates.render_to_string("index.html") == "hello"
+
+    templates = Jinja2Templates(template_dir=[str(tmp_path)])
+    assert templates.render_to_string("index.html") == "hello"
+
+
+def test_accepts_packages(jinja_env: jinja2.Environment) -> None:
+    templates = Jinja2Templates(packages=["tests.assets"])
+    assert templates.render_to_string("index.html") == "hello"
+
+
+def test_accepts_custom_loader(jinja_env: jinja2.Environment, tmp_path: pathlib.Path) -> None:
+    (tmp_path / "index.html").write_text("hello")
+    loader = jinja2.FileSystemLoader(tmp_path)
+    templates = Jinja2Templates(loader=loader)
+    assert templates.render_to_string("index.html") == "hello"
+
+
+def test_accepts_custom_filters(jinja_env: jinja2.Environment, templates: pathlib.Path) -> None:
+    def example(value: str) -> str:
+        return "filtered"
+
+    (templates / "index.html").write_text('{{ "key"|example }}')
+    templates = Jinja2Templates(env=jinja_env, filters={"example": example})
+    assert templates.render_to_string("index.html") == "filtered"
+
+
+def test_accepts_custom_tests(jinja_env: jinja2.Environment, templates: pathlib.Path) -> None:
+    def is_true(value: str) -> bool:
+        return value == "true"
+
+    (templates / "index.html").write_text('{{ "key" is is_true }}-{{ "true" is is_true }}')
+    templates = Jinja2Templates(env=jinja_env, tests={"is_true": is_true})
+    assert templates.render_to_string("index.html") == "False-True"
+
+
+def test_accepts_custom_globals(jinja_env: jinja2.Environment, templates: pathlib.Path) -> None:
+    (templates / "index.html").write_text("{{ key }}")
+    templates = Jinja2Templates(env=jinja_env, globals={"key": "value"})
+    assert templates.render_to_string("index.html") == "value"
+
+
+def test_integrates_with_starlette_babel(jinja_env: jinja2.Environment, templates: pathlib.Path) -> None:
+    (templates / "index.html").write_text("{{ key }}")
+    templates = Jinja2Templates(env=jinja_env, globals={"key": "value"})
+    assert templates.render_to_string("index.html") == "value"
+
+
+def test_custom_plugins(jinja_env: jinja2.Environment, templates: pathlib.Path) -> None:
+    def plugin(env: jinja2.Environment) -> None:
+        env.globals.update({"key": "value"})
+
+    (templates / "index.html").write_text("{{ key }}")
+    templates = Jinja2Templates(env=jinja_env, plugins=[plugin])
+    assert templates.render_to_string("index.html") == "value"
+
+
+def test_context_processor_decorator(jinja_env: jinja2.Environment, templates: pathlib.Path) -> None:
+    (templates / "index.html").write_text("{{ key }}")
+    request = Request(scope={"type": "http"})
+    templates = Jinja2Templates(env=jinja_env)
+
+    @templates.context_processor
+    def processor(request: Request) -> dict[str, typing.Any]:
+        return {"key": "value"}
+
+    response = templates.TemplateResponse(request, "index.html")
+    assert response.body == b"value"
+
+
+def test_filter_decorator(jinja_env: jinja2.Environment, templates: pathlib.Path) -> None:
+    (templates / "index.html").write_text("{{ key|value }}")
+    request = Request(scope={"type": "http"})
+    templates = Jinja2Templates(env=jinja_env)
+
+    @templates.filter()
+    def value(value: str) -> str:
+        return "filtered"
+
+    response = templates.TemplateResponse(request, "index.html")
+    assert response.body == b"filtered"
+
+
+def test_filter_decorator_with_custom_name(jinja_env: jinja2.Environment, templates: pathlib.Path) -> None:
+    (templates / "index.html").write_text("{{ key|to_value }}")
+    request = Request(scope={"type": "http"})
+    templates = Jinja2Templates(env=jinja_env)
+
+    @templates.filter("to_value")
+    def value(value: str) -> str:
+        return "filtered"
+
+    response = templates.TemplateResponse(request, "index.html")
+    assert response.body == b"filtered"
+
+
+def test_test_decorator(jinja_env: jinja2.Environment, templates: pathlib.Path) -> None:
+    (templates / "index.html").write_text('{{ "true" is is_bool }}')
+    request = Request(scope={"type": "http"})
+    templates = Jinja2Templates(env=jinja_env)
+
+    @templates.test()
+    def is_bool(value: str) -> bool:
+        return value == "true"
+
+    response = templates.TemplateResponse(request, "index.html")
+    assert response.body == b"True"
+
+
+def test_test_decorator_with_custom_name(jinja_env: jinja2.Environment, templates: pathlib.Path) -> None:
+    (templates / "index.html").write_text('{{ "true" is is_boolean }}')
+    request = Request(scope={"type": "http"})
+    templates = Jinja2Templates(env=jinja_env)
+
+    @templates.test("is_boolean")
+    def is_bool(value: str) -> bool:
+        return value == "true"
+
+    response = templates.TemplateResponse(request, "index.html")
+    assert response.body == b"True"
+
+
+def test_starlette_setup(templates: pathlib.Path) -> None:
+    app = Starlette()
+    templates = Jinja2Templates(template_dir=templates)
+    templates.setup(app)
+    assert isinstance(app.state.templates, Jinja2Templates)
+    assert isinstance(app.state.jinja_env, jinja2.Environment)
+
+
+def test_media_url() -> None:
+    app = Starlette(
+        routes=[
+            Mount("/media", simple_asgi_app, name="media"),
+            Mount("/custom-media", simple_asgi_app, name="custom_media"),
+        ]
+    )
+    request = Request(scope={"type": "http", "app": app})
+    assert media_url(request, "file.txt") == "/media/file.txt"
+    assert media_url(request, "http://localhost/file.txt") == "http://localhost/file.txt"
+    assert media_url(request, "file.txt", path_name="custom_media") == "/custom-media/file.txt"
+
+
+def test_static_url() -> None:
+    app = Starlette(
+        routes=[
+            Mount("/static", simple_asgi_app, name="static"),
+            Mount("/custom-static", simple_asgi_app, name="custom_static"),
+        ]
+    )
+    request = Request(scope={"type": "http", "app": app})
+    assert static_url(request, "file.txt", append_timestamp=False) == "/static/file.txt"
+    assert static_url(request, "file.txt").startswith("/static/file.txt?ts=")
+    assert (
+        static_url(request, "file.txt", append_timestamp=False, path_name="custom_static") == "/custom-static/file.txt"
+    )
+
+
+def test_url_for() -> None:
+    app = Starlette(
+        routes=[
+            Mount("/media", simple_asgi_app, name="media"),
+        ]
+    )
+    request = Request(scope={"type": "http", "app": app})
+    assert url_for(request, "media", path="file") == "/media/file"
+
+
+def test_abs_url_for() -> None:
+    app = Starlette(
+        routes=[
+            Mount("/media", simple_asgi_app, name="media"),
+        ]
+    )
+    request = Request(
+        scope={
+            "type": "http",
+            "app": app,
+            "router": app.router,
+            "scheme": "http",
+            "root_path": "",
+            "headers": ((b"host", b"testserver"),),
+        }
+    )
+    assert str(abs_url_for(request, "media", path="file")) == "http://testserver/media/file"
+
+
+def test_url_matches() -> None:
+    app = Starlette(
+        routes=[
+            Mount("/media", simple_asgi_app, name="media"),
+        ]
+    )
+    request = Request(scope={"type": "http", "app": app, "root_path": "/media/file", "path": "", "headers": []})
+    assert url_matches(request, "media", path_params={"path": "file"})
+    assert url_matches(request, "media", path_params={"path": "file"}, exact=True)
+    assert not url_matches(request, "media", path_params={"path": "file2"})
+    assert url_matches(request, "media", path_params={"path": "fil"})
+    assert not url_matches(request, "media", path_params={"path": "fil"}, exact=True)
