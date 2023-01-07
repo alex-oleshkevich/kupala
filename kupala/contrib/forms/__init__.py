10449bebdf1a4f0e3f1fbd5e4fa1cefee53eb2a2
alex.oleshkevich <alex.oleshkevich@gmail.com>
2023-01-07 18:30:20 +0300

    export default types

---
+++ kupala/contrib/forms/__init__.py
@@ -0,0 +1,4 @@
+from kupala.contrib.forms.forms import AsyncForm, Preparable
+from kupala.contrib.forms.validators import AsyncValidator, SyncValidator, Validator
+
+__all__ = ["AsyncForm", "Preparable", "SyncValidator", "AsyncValidator", "Validator"]
