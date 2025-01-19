06c08d8934ddfdc81540666e9a58f5dec9cd3f50
alex.oleshkevich <alex.oleshkevich@gmail.com>
2025-01-19 18:26:48 +0100

    setup

--- kupala/extensions.py
+++
@@ -1,9 +0,0 @@
-import abc
-import typing
-from starlette.applications import AppType
-
-
-class Extension:  # pragma: nocover
-    @abc.abstractmethod
-    def bootstrap(self, app: AppType) -> typing.AsyncContextManager[typing.Mapping[str, typing.Any] | None]:
-        ...
--- tests/console/test_console_app_factory.py
+++
@@ -1,8 +0,0 @@
-from starlette.applications import Starlette
-
-from kupala.console import Group, create_console_app
-
-
-def test_create_console_app() -> None:
-    app = create_console_app(Starlette())
-    assert isinstance(app, Group)
