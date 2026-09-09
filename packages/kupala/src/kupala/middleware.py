3cbb79cf3d4f5abcdb058e89a0749b2d698e3fe7
alex.oleshkevich <alex.oleshkevich@gmail.com>
2026-09-09 10:29:17 +0200

    Support variadic middleware arguments

--- packages/kupala/src/kupala/middleware.py
+++ packages/kupala/src/kupala/middleware.py
@@ -5,10 +5,13 @@ from kupala.responses import Response
 from kupala.websockets import WebSocket
 
 type CallNext = typing.Callable[[Request], typing.Awaitable[Response]]
-type Middleware = typing.Callable[[Request, CallNext], typing.Awaitable[Response]]
-
 type WebSocketCallNext = typing.Callable[[WebSocket], typing.Awaitable[None]]
+
+type Middleware = typing.Callable[
+    typing.Concatenate[Request, CallNext, ...],
+    typing.Awaitable[Response],
+]
 type WebSocketMiddleware = typing.Callable[
-    [WebSocket, WebSocketCallNext],
+    typing.Concatenate[WebSocket, WebSocketCallNext, ...],
     typing.Awaitable[None],
 ]
